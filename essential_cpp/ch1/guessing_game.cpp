af8186f61361b14a330e926fd30e717ac4fa909e
Willendless <317500141@qq.com>
2020-07-13 21:09:21 -0700

    fix: update format

--- essential_cpp/ch1/guessing_game.cpp
+++ essential_cpp/ch1/guessing_game.cpp
@@ -10,14 +10,14 @@ string user_name;
 bool next_turn = true;
 
 int num_guess = 0, 
-	num_right = 0;
+    num_right = 0;
 
 double guess_score = 0.0;
 
 const int seq_size = 18;
 int elem_vals[seq_size] = {
-	1, 2, 3, 3, 4, 7, 2, 5, 12,
-	3, 6, 10, 4, 9, 16, 5, 12, 22
+    1, 2, 3, 3, 4, 7, 2, 5, 12,
+    3, 6, 10, 4, 9, 16, 5, 12, 22
 };
 /*
 vector<int> pell_seq(elem_vals, elem_vals + seq_size);
@@ -25,106 +25,110 @@ vector<int> pell_seq(elem_vals, elem_vals + seq_size);
 
 const int seq_cnt = 6;
 vector<int> fibonacci(elem_vals, elem_vals + 3),
-			lucas(elem_vals + 3, elem_vals + 6),
-			pell(elem_vals + 6, elem_vals + 9),
-			triangle(elem_vals + 9, elem_vals + 12),
-			square(elem_vals + 12, elem_vals + 15),
-			pentagonal(elem_vals + 15, elem_vals + 18);
+            lucas(elem_vals + 3, elem_vals + 6),
+            pell(elem_vals + 6, elem_vals + 9),
+            triangle(elem_vals + 9, elem_vals + 12),
+            square(elem_vals + 12, elem_vals + 15),
+            pentagonal(elem_vals + 15, elem_vals + 18);
 vector<int> *seq_addrs[seq_cnt] = {
-	&fibonacci, &lucas, &pell,
+    &fibonacci, &lucas, &pell,
 	&triangle, &square, &pentagonal
 };
 
 int
 main()
 {
-	cout << "Please enter your user name:";
-	cin >> user_name;
+    cout << "Please enter your user name:";
+    cin >> user_name;
 
-	ifstream infile("guessing_game.txt");
+    ifstream infile("guessing_game.txt", ios_base::in);
 
-	if (!infile) {
-		cout << "Welcome! New user" << user_name << "\n";
-	} else {
-		string name;
-		int ng;
-		int nr;
-		bool found = false;
-		while (infile >> name) {
-			infile >> ng >> nr;
-			if (name == user_name) {
-				cout << "Welcome back, " << user_name
-					 << "\nYour current score is " << nr
-					 << " out of " << ng << "\nGood luck!\n";
-				num_guess = ng;
-				num_right = nr;
-				found = true;
-				break;
-			}
-		}
-		if (!found)
-			cout << "Welcome! New user, " << user_name << "\n";
-	}
+    if (!infile) {
+        cerr << "Failed open file guessing_game" << endl;
+        exit(-1);
+    } else {
+        infile.seekg(0);
+        string name;
+        int ng;
+        int nr;
+        bool found = false;
+        while (infile >> name) {
+            infile >> ng >> nr;
+            if (name == user_name) {
+                num_guess = ng;
+                num_right = nr;
+                found = true;
+            }
+        }
+        if (!found)
+            cout << "Welcome! New user, " << user_name << "\n";
+        else {
+            cout << "Welcome back, " << user_name
+                 << "\nYour current score is " << num_right
+                 << " out of " << num_right<< "\nGood luck!\n";
+        }
+    }
 
-	//int cur_tuple = 0;
-	srand(seq_cnt);
-	ofstream outfile("guessing_game.txt", ios_base::app);
-	//while (next_turn && cur_tuple < seq_size) {
-	while (next_turn) {
-		bool guess_correct = false;
-		bool next_guess = true;
-		int  seq_index = rand() % seq_cnt;
-		vector<int> *cur_vec = seq_addrs[seq_index];
+    //int cur_tuple = 0;
+    srand(seq_cnt);
+    //while (next_turn && cur_tuple < seq_size) {
+    while (next_turn) {
+        bool guess_correct = false;
+        bool next_guess = true;
+        int  seq_index = rand() % seq_cnt;
+        vector<int> *cur_vec = seq_addrs[seq_index];
 
-		/*
-		cout << "The first two element are:"
-			 << pell_seq[cur_tuple] << ", "
-			 << pell_seq[cur_tuple + 1]
-			 << "\nWhat is your guess?";
-		*/
+        /*
+        cout << "The first two element are:"
+        	 << pell_seq[cur_tuple] << ", "
+        	 << pell_seq[cur_tuple + 1]
+        	 << "\nWhat is your guess?";
+        */
 
 
-		while (next_guess && (!guess_correct)) {
-			int guess_val;
-			cout << "The first two element are:"
-				 << (*cur_vec)[0] << ", "
-				 << (*cur_vec)[1]
-				 << "\nWhat is your guess?";
-			cin >> guess_val;
-			num_guess++;
-			if (guess_val == (*cur_vec)[2]) {
-				cout << "Correct!\n";
-				guess_correct = true;
-				num_right++;
-			} else {
-				char user_rsp;
-				cout << "Wrong! Continue guess? (Y/N)\n";
-				cin >> user_rsp;
-				if (user_rsp == 'N' || user_rsp == 'n')
-					next_guess = false;
-			}
-		}
+        while (next_guess && (!guess_correct)) {
+            int guess_val;
+            cout << "The first two element are:"
+                 << (*cur_vec)[0] << ", "
+                 << (*cur_vec)[1]
+                 << "\nWhat is your guess?";
+            cin >> guess_val;
+            num_guess++;
+            if (guess_val == (*cur_vec)[2]) {
+                cout << "Correct!\n";
+                guess_correct = true;
+                num_right++;
+            } else {
+                char user_rsp;
+                cout << "Wrong! Continue guess? (Y/N)\n";
+                cin >> user_rsp;
+                if (user_rsp == 'N' || user_rsp == 'n')
+                    next_guess = false;
+            }
+        }
 
-		cout << "Want to try another sequence? (Y/N)";
-		char try_again;
-		cin >> try_again;
+        cout << "Want to try another sequence? (Y/N)";
+        char try_again;
+        cin >> try_again;
 
-		if (try_again == 'N' || try_again == 'n')
-			next_turn = false;
+        if (try_again == 'N' || try_again == 'n')
+        	next_turn = false;
 
-		//cur_tuple += 3;
-	}
+        //cur_tuple += 3;
+    }
 
-	cout << "Guess num: " << num_guess
-		 << ", Guess correct num: " << num_right 
-		 << "\nGuess correct rate: " << 1.0 * num_right / num_guess * 100 << "%\n";
+    ofstream outfile("guessing_game.txt", ios_base::app);
 
-	if (!outfile)
-		cerr << "Oops! Unable to save session data!\n";
-	else
-		outfile << user_name << ' '
-			    << num_guess << ' '
-				<< num_right << endl;
-	return 0;
+    cout << "Guess num: " << num_guess
+         << ", Guess correct num: " << num_right 
+         << "\nGuess correct rate: " << 1.0 * num_right / num_guess * 100 << "%\n";
+
+    if (!outfile)
+        cout << "Oops! Unable to save session data!\n";
+    else {
+        outfile << user_name << ' '
+                << num_guess << ' '
+                << num_right << endl;
+    }
+    return 0;
 }
-
--- essential_cpp/ch1/hello_world.cpp
+++ essential_cpp/ch1/hello_world.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int
 main()
 {
-	string user_name;
-	cout << "Please input your name: ";
-	cin >> user_name;
-	cout << "\n";
+    string user_name;
+    cout << "Please input your name: ";
+    cin >> user_name;
+    cout << "\n";
 
-	cout << "Hello world, "
-		 << user_name
-		 << "... and goodbye!"
-		 << "\n";
+    cout << "Hello world, "
+         << user_name
+         << "... and goodbye!"
+         << "\n";
 
-	return 0;
+    return 0;
 }
-
